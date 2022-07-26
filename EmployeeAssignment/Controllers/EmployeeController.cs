2cf99ec5eba88eb20062d8f462afdff793da77ec
Dipakshi26 <dipakshisaxena26@gmail.com>
2022-07-26 15:55:52 +0530

    Add project files.

---
+++ EmployeeAssignment/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAssignment.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
